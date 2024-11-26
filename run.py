100b209f58c5160ac8bcb9503cb7f6ec44377ac5
AinzOoalGowns <elaineseraphina.eth@gmail.com>
2024-11-26 14:17:05 +0700

    Add files via upload

---
+++ run.py
@@ -0,0 +1,6 @@
+import sys
+sys.path.append('build/lib.linux-aarch64-cpython-312')  # Menambahkan folder ke path
+import script1  # Mengimpor modul yang telah dikompilasi
+
+# Panggil fungsi atau eksekusi kode yang ada di dalam script1
+script1.main()  # Jika ada fungsi main atau fungsi lainnya di dalam script1
