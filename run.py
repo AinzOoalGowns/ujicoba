41117b5851e294d02da7d2222b39bfc9e907a7c2
AinzOoalGowns <elaineseraphina.eth@gmail.com>
2024-11-26 14:18:53 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import sys
+import asyncio  # Pastikan asyncio diimpor
 sys.path.append('build/lib.linux-aarch64-cpython-312')  # Menambahkan folder ke path
 import script1  # Mengimpor modul yang telah dikompilasi
 
-# Panggil fungsi atau eksekusi kode yang ada di dalam script1
-script1.main()  # Jika ada fungsi main atau fungsi lainnya di dalam script1
+# Menjalankan fungsi main yang merupakan coroutine
+asyncio.run(script1.main())  # Gunakan asyncio.run untuk menjalankan coroutine
